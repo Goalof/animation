72dc471cec0f3f8d45dd3941b338272f37081cd6
Goalof <68434460+Goalof@users.noreply.github.com>
2020-09-01 17:35:45 +0600

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,13 +40,13 @@ export default (() => {
 				text-transform="uppercase"
 				letter-spacing="5px"
 			>
-				Second to none
+				Second to none/
 			</Text>
 			<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0 0 0">
 				About Us
 			</Text>
 			<Text as="p" font="--lead" margin="40px 0 20px 0">
-				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
 		</Section>
 		<Section
@@ -206,6 +206,128 @@ export default (() => {
 							</Stack>
 						</Formspree>
 					</Box>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						color="--dark"
+						padding="98px 64px"
+						background="--color-light"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="12px 0"
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About Zanzibar
+					</Text>
+					<Text as="h2" margin="12px 0" font="--headline2" md-font="--headline3">
+						This is one of the world's greatest cultural crossroads where Africa meets the Indian Ocean.
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Stack>
+						<StackItem width="100%" lg-width="33.3%" md-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999080555-3f7a698dd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1577315734214-4b3dec92d9ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999502951-4ab25b5ca889?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80) center/cover" />
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Stack>
+				<StackItem width="50%" lg-width="100%" lg-order="2">
+					<Override slot="StackItemContent" text-align="center" />
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Nathan K. Joe
+							</Text>
+							<Text color="--grey" margin="0">
+								CEO
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1546672741-d327539d5f13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								David R. Lema
+							</Text>
+							<Text color="--grey" margin="0">
+								Developer
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Janet E. Morey
+							</Text>
+							<Text color="--grey" margin="0">
+								Marketing
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Claire R. Peery
+							</Text>
+							<Text color="--grey" margin="0">
+								Designer
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" lg-margin-bottom="24px" flex-direction="column" />
+					<Text color="--grey" letter-spacing="1px" text-transform="uppercase" margin="0 0 10px 0">
+						Meet our team
+					</Text>
+					<Text as="h2" font="--headline2" md-font="--headline3" margin="0">
+						The smartest people work every day to provide the best service and to make our clients happy
+					</Text>
 				</StackItem>
 			</Stack>
 		</Section>
