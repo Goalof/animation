9fdf26e484f437b8f54eddede66ababd269f8458
Goalof <68434460+Goalof@users.noreply.github.com>
2020-09-01 17:33:13 +0600

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ export default (() => {
 				About Us
 			</Text>
 			<Text as="p" font="--lead" margin="40px 0 20px 0">
-				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
 		</Section>
 		<Section
